64f78a43dec8fd651a23d946a6052517f1a4c67e
Techie <techiesaumya@gmail.com>
2024-11-02 20:12:05 +0530

    Forget Password Screen UI Completed

--- lib/shared/screens/forget_password_screen.dart
+++ lib/shared/screens/forget_password_screen.dart
@@ -1,4 +1,6 @@
 import 'package:aayu_app/core/themes/app_colors.dart';
+import 'package:aayu_app/shared/components/primary_button.dart';
+import 'package:aayu_app/shared/components/primary_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,35 +10,67 @@ class ForgetPasswordScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(
-                    Icons.chevron_left,
-                    size: 32.sp,
-                    color: AppColors.titleHeadingColor,
-                  ),
+        backgroundColor: AppColors.primaryWhiteColor,
+        appBar: AppBar(
+          title: Text(
+            "Forget Password",
+            style: GoogleFonts.poppins(
+              fontSize: 20.sp,
+              color: AppColors.titleHeadingColor,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.w600,
+              height: 1.3,
+            ),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.chevron_left,
+              size: 32.sp,
+              color: AppColors.titleHeadingColor,
+            ),
+          ),
+          backgroundColor: AppColors.primaryWhiteColor,
+        ),
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.only(bottom: 10.h, left: 5.w, right: 5.w),
+          child: PrimaryButton(
+            text: 'Verify Email',
+            onPressed: () {},
+            width: double.infinity,
+            height: 50.h,
+            buttonColor: AppColors.primaryBrownColor,
+            buttonTextColor: AppColors.primaryWhiteColor,
+          ),
+        ),
+        body: Container(
+          margin: EdgeInsets.fromLTRB(20.w, 10.h, 10.w, 10.h),
+          child: Column(
+            children: [
+              SizedBox(height: 10.h),
+              Text(
+                'Enter your registered email address in order to receive a one-time password (OTP) for resetting your password.',
+                style: GoogleFonts.poppins(
+                  fontSize: 14.sp,
+                  color: AppColors.bodyNeutralColor,
+                  fontStyle: FontStyle.normal,
+                  height: 1.5,
                 ),
-                Text(
-                  "Forget Password",
-                  style: GoogleFonts.poppins(
-                    fontSize: 20.sp,
-                    color: AppColors.titleHeadingColor,
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.w600,
-                    height: 1.3,
-                  ),
-                ),
-              ],
-            )
-          ],
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 50.h),
+              PrimaryTextField(
+                label: "Email",
+                hintText: 'Enter Email',
+                controller: emailController,
+              ),
+            ],
+          ),
         ),
       ),
     );
